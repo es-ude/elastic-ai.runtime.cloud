5107926259f82b005e85777951ebafff00a5b14a
Maximilian Fischer <maximilian.fischer@uni-due.de>
2022-10-20 18:44:29 +0200

    add argument parser for monitor execution (#19)
    * add argument parser for monitor execution
    
    * update error handling of argument parser
    
    * Prettified Code!
    
    * add workflow manual trigger
    
    * change gradle execution to bundled wrapper
    
    * fix integrationTest - 01
    
    * fix integrationTest - 02
    
    * fix integrationTest - 03
    
    * fix integrationTest - 04
    
    integration Tests should be run separately
    
    Co-authored-by: David P. Federl <david-peter.federl@stud.uni-due.de>

--- monitor/src/main/java/de/ude/es/Main.java
+++ monitor/src/main/java/de/ude/es/Main.java
@@ -2,16 +2,34 @@ package de.ude.es;
 
 import de.ude.es.comm.HivemqBroker;
 import de.ude.es.twin.JavaTwin;
+import java.sql.SQLOutput;
+import java.util.Objects;
+import net.sourceforge.argparse4j.ArgumentParsers;
+import net.sourceforge.argparse4j.inf.ArgumentGroup;
+import net.sourceforge.argparse4j.inf.ArgumentParser;
+import net.sourceforge.argparse4j.inf.ArgumentParserException;
+import net.sourceforge.argparse4j.inf.Namespace;
 
 public class Main {
 
+    private static String BROKER = null;
+    private static Integer PORT = null;
     public static TwinList twinList;
     private static final String DOMAIN = "eip://uni-due.de/es";
     private static final int kikTime = 60000;
 
     public static void main(String[] args) {
+        try {
+            Namespace arguments = parseArguments(args);
+            BROKER = arguments.getString("broker_address");
+            PORT = Integer.parseInt(arguments.getString("broker_port"));
+        } catch (ArgumentParserException exception) {
+            System.out.println(exception.getMessage());
+            System.exit(10);
+        }
+
         twinList = new TwinList(kikTime);
-        HivemqBroker broker = new HivemqBroker(DOMAIN);
+        HivemqBroker broker = new HivemqBroker(DOMAIN, BROKER, PORT);
 
         var sink = new JavaTwin("monitor");
         sink.bind(broker);
@@ -23,5 +41,29 @@ public class Main {
 
         MonitoringServiceApplication serviceApplication = new MonitoringServiceApplication();
         serviceApplication.startServer(args);
+    }
+
+    public static Namespace parseArguments(String[] args)
+        throws ArgumentParserException {
+        ArgumentParser parser = ArgumentParsers
+            .newFor("elastic.ai-monitor")
+            .build()
+            .defaultHelp(true)
+            .description(
+                "Service for monitoring digital twins in the elastic-ai.runtime"
+            );
+        ArgumentGroup brokerSpecification = parser.addArgumentGroup(
+            "MQTT Broker Specification"
+        );
+        brokerSpecification
+            .addArgument("-b", "--broker-address")
+            .help("Broker Address")
+            .setDefault("localhost");
+        brokerSpecification
+            .addArgument("-p", "--broker-port")
+            .help("Broker Port")
+            .setDefault(1883);
+
+        return parser.parseArgs(args);
     }
 }
