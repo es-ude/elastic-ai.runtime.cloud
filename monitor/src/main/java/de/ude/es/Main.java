093d586123531188c62fe8a963d0b44c6684bff5
David P. Federl <david-peter.federl@stud.uni-due.de>
2022-10-20 13:59:52 +0200

    update error handling of argument parser

--- monitor/src/main/java/de/ude/es/Main.java
+++ monitor/src/main/java/de/ude/es/Main.java
@@ -1,14 +1,17 @@
 package de.ude.es;
 
+
 import de.ude.es.comm.HivemqBroker;
 import de.ude.es.twin.JavaTwin;
 import net.sourceforge.argparse4j.ArgumentParsers;
+import net.sourceforge.argparse4j.inf.ArgumentGroup;
 import net.sourceforge.argparse4j.inf.ArgumentParser;
 import net.sourceforge.argparse4j.inf.ArgumentParserException;
 import net.sourceforge.argparse4j.inf.Namespace;
 
 import java.sql.SQLOutput;
 import java.util.Objects;
+
 
 public class Main {
 
@@ -19,11 +22,14 @@ public class Main {
     private static final int kikTime = 60000;
 
     public static void main(String[] args) {
-
-        Namespace arguments = parseArguments(args);
-
-        BROKER = arguments.getString("broker_address");
-        PORT = Integer.parseInt(arguments.getString("broker_port"));
+        try {
+            Namespace arguments = parseArguments(args);
+            BROKER = arguments.getString("broker_address");
+            PORT = Integer.parseInt(arguments.getString("broker_port"));
+        } catch (ArgumentParserException exception) {
+            System.out.println(exception.getMessage());
+            System.exit(10);
+        }
 
         twinList = new TwinList(kikTime);
         HivemqBroker broker = new HivemqBroker(DOMAIN, BROKER, PORT);
@@ -38,18 +44,15 @@ public class Main {
         serviceApplication.startServer(args);
     }
 
-    public static Namespace parseArguments(String[] args) {
+    public static Namespace parseArguments(String[] args) throws ArgumentParserException {
         ArgumentParser parser = ArgumentParsers.newFor("elastic.ai-monitor")
                 .build()
                 .defaultHelp(true)
                 .description("Service for monitoring digital twins in the elastic-ai.runtime");
-        parser.addArgument("-b", "--broker-address").help("Broker Address").setDefault("localhost");
-        parser.addArgument("-p", "--broker-port").help("Broker Port").setDefault(1883);
-        Namespace parsedArguments = null;
-        try {
-            parsedArguments = parser.parseArgs(args);
-        } catch (ArgumentParserException e) {
-        }
-        return parsedArguments;
+        ArgumentGroup brokerSpecification = parser.addArgumentGroup("MQTT Broker Specification");
+        brokerSpecification.addArgument("-b", "--broker-address").help("Broker Address").setDefault("localhost");
+        brokerSpecification.addArgument("-p", "--broker-port").help("Broker Port").setDefault(1883);
+
+        return parser.parseArgs(args);
     }
 }
