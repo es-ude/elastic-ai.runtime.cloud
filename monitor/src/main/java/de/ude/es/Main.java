e9bad45a2a28cfe53a320660f1c4681ce249400b
DavidFederl <DavidFederl@users.noreply.github.com>
2022-10-20 13:12:37 +0000

    Prettified Code!

--- monitor/src/main/java/de/ude/es/Main.java
+++ monitor/src/main/java/de/ude/es/Main.java
@@ -1,17 +1,14 @@
 package de.ude.es;
 
-
 import de.ude.es.comm.HivemqBroker;
 import de.ude.es.twin.JavaTwin;
+import java.sql.SQLOutput;
+import java.util.Objects;
 import net.sourceforge.argparse4j.ArgumentParsers;
 import net.sourceforge.argparse4j.inf.ArgumentGroup;
 import net.sourceforge.argparse4j.inf.ArgumentParser;
 import net.sourceforge.argparse4j.inf.ArgumentParserException;
 import net.sourceforge.argparse4j.inf.Namespace;
-
-import java.sql.SQLOutput;
-import java.util.Objects;
-
 
 public class Main {
 
@@ -37,21 +34,35 @@ public class Main {
         var sink = new JavaTwin("monitor");
         sink.bind(broker);
 
-        HeartbeatSubscriber heartbeatSubscriber = new HeartbeatSubscriber(twinList);
+        HeartbeatSubscriber heartbeatSubscriber = new HeartbeatSubscriber(
+            twinList
+        );
         heartbeatSubscriber.bind(broker);
 
         MonitoringServiceApplication serviceApplication = new MonitoringServiceApplication();
         serviceApplication.startServer(args);
     }
 
-    public static Namespace parseArguments(String[] args) throws ArgumentParserException {
-        ArgumentParser parser = ArgumentParsers.newFor("elastic.ai-monitor")
-                .build()
-                .defaultHelp(true)
-                .description("Service for monitoring digital twins in the elastic-ai.runtime");
-        ArgumentGroup brokerSpecification = parser.addArgumentGroup("MQTT Broker Specification");
-        brokerSpecification.addArgument("-b", "--broker-address").help("Broker Address").setDefault("localhost");
-        brokerSpecification.addArgument("-p", "--broker-port").help("Broker Port").setDefault(1883);
+    public static Namespace parseArguments(String[] args)
+        throws ArgumentParserException {
+        ArgumentParser parser = ArgumentParsers
+            .newFor("elastic.ai-monitor")
+            .build()
+            .defaultHelp(true)
+            .description(
+                "Service for monitoring digital twins in the elastic-ai.runtime"
+            );
+        ArgumentGroup brokerSpecification = parser.addArgumentGroup(
+            "MQTT Broker Specification"
+        );
+        brokerSpecification
+            .addArgument("-b", "--broker-address")
+            .help("Broker Address")
+            .setDefault("localhost");
+        brokerSpecification
+            .addArgument("-p", "--broker-port")
+            .help("Broker Port")
+            .setDefault(1883);
 
         return parser.parseArgs(args);
     }
