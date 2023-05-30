5dd15e1e076e6c37bd806216af7c3cf1f49c66c4
David P. Federl <david-peter.federl@stud.uni-due.de>
2023-05-30 12:09:20 +0200

    fix(wtest): trigger release wf

--- monitor/src/main/java/de/ude/es/workflow_trigger.java
+++ monitor/src/main/java/de/ude/es/workflow_trigger.java
@@ -1,8 +1,6 @@
 // Your First Program
 
 class HelloWorld {
-
-    public static void main(String[] args) {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) {System.out.println("Hello, World!");
     }
 }
