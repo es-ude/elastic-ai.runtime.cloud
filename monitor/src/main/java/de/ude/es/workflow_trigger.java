fe09eb7b91ade835df72069ba0e886af0c8efe61
DavidFederl <DavidFederl@users.noreply.github.com>
2023-05-30 10:09:41 +0000

    style: beautify 7a87b7de836dc4a18245de9fbaebe4fab82a1df3

--- monitor/src/main/java/de/ude/es/workflow_trigger.java
+++ monitor/src/main/java/de/ude/es/workflow_trigger.java
@@ -1,6 +1,8 @@
 // Your First Program
 
 class HelloWorld {
-    public static void main(String[] args) {System.out.println("Hello, World!");
+
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
     }
 }
