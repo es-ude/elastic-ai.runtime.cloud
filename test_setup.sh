9244ef82993283a0f13a559c1d981b59372b675c
David P. Federl <david-peter.federl@stud.uni-due.de>
2023-06-12 12:05:51 +0200

    test(environment): add bash script to test dependencies

---
+++ test_setup.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+if [[ -z $(command -v docker) ]]; then
+  UNDEFINED_DEPENDENCY_FOUND=1
+  echo "docker not found! Please install docker."
+fi
+
+# gradle doesn't need to be checked -> gradle wrapper provided!
+
+if [[ -z $(command -v java) ]]; then
+  UNDEFINED_DEPENDENCY_FOUND=1
+  echo "Java not found! Please install JDK version 17 or higher."
+elif [[ -z $(java --version | awk '/openjdk/ && ($2 >= 17) {print "match"}') ]]; then
+  UNDEFINED_DEPENDENCY_FOUND=1
+  echo "Wrong version of Java found! Please install JDK version 17 or higher."
+fi
+
+if [[ $UNDEFINED_DEPENDENCY_FOUND -ne 1 ]]; then
+  echo "All dependencies are satisfied!"
+fi
+
+if [[ -z "$HOST_IP" ]]; then
+  echo ""
+  echo "HOST_IP not defined. Please define a environment variable that stores your current hosts IP."
+  echo "For example: Add 'export HOST_IP=\$(hostname -I)' to your .bashrc"
+fi
