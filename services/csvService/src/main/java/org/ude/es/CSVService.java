9e0ac9814f29438444a86d498fece0126915c32d
DerDavidos <david.lewakis@stud.uni-due.de>
2024-01-11 23:22:35 +0100

    refactor(ballChallenge): change csv generation

--- services/csvService/src/main/java/org/ude/es/CSVService.java
+++ services/csvService/src/main/java/org/ude/es/CSVService.java
@@ -36,29 +36,23 @@ public class CSVService extends LocalCommunicationEndpoint {
         File sensorValueDir = new File(PATH);
         sensorValueDir.mkdir();
 
-        String fileName =
-            PATH + "/" + new Timestamp(System.currentTimeMillis());
-        try {
-            FileWriter csvHeader = new FileWriter(fileName, false);
-            csvHeader.append("timestamp");
-            csvHeader.append(",");
-            csvHeader.append("g-value");
-            csvHeader.append("\n");
-            csvHeader.flush();
-            csvHeader.close();
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-
         dataRequester.setDataReceiveFunction(data -> {
+            String fileName =
+                    PATH + "/" + new Timestamp(System.currentTimeMillis());
             try {
-                FileWriter csvWriter = new FileWriter(fileName, true);
-                csvWriter.append(
-                    new Timestamp(System.currentTimeMillis()).toString()
-                );
+                System.out.println(fileName);
+                File csvDir = new File(fileName);
+                csvDir.mkdir();
+
+                FileWriter csvWriter = new FileWriter(fileName + "/measurement.csv", false);
+                csvWriter.append("x");
                 csvWriter.append(",");
-                csvWriter.append(data);
+                csvWriter.append("y");
+                csvWriter.append(",");
+                csvWriter.append("z");
                 csvWriter.append("\n");
+
+                csvWriter.append(data.replace(";", "\n"));
                 csvWriter.flush();
                 csvWriter.close();
             } catch (IOException e) {
