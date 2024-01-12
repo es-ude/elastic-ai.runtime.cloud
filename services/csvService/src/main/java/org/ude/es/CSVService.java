21594e99dfedca4d92634fe5c3b143a0bc302da3
DerDavidos <david.lewakis@stud.uni-due.de>
2024-01-12 15:35:12 +0100

    feat(ballChallenge): take pictures with ip cam when data received

--- services/csvService/src/main/java/org/ude/es/CSVService.java
+++ services/csvService/src/main/java/org/ude/es/CSVService.java
@@ -1,9 +1,12 @@
 package org.ude.es;
 
-import java.io.File;
-import java.io.FileWriter;
-import java.io.IOException;
+import java.io.*;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.sql.Timestamp;
+
 import org.ude.es.communicationEndpoints.LocalCommunicationEndpoint;
 import org.ude.es.communicationEndpoints.RemoteCommunicationEndpoint;
 import org.ude.es.protocol.requests.DataRequester;
@@ -23,18 +26,28 @@ public class CSVService extends LocalCommunicationEndpoint {
         enV5Twin = new RemoteCommunicationEndpoint("enV5Twin");
     }
 
+    private void savePicture(String filePath) {
+        try(InputStream in = new URL("http://192.168.203.24:8081/jpeg").openStream()){
+            Files.copy(in, Paths.get(filePath + "/image.jpg"));
+        } catch (MalformedURLException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            System.out.println("NO PICTURE TAKEN!!!");
+        }
+    }
+
     @Override
     protected void executeOnBind() {
         enV5Twin.bindToCommunicationEndpoint(brokerStub);
+
+        File sensorValueDir = new File(PATH);
+        sensorValueDir.mkdir();
 
         DataRequester dataRequester = new DataRequester(
             enV5Twin,
             "g-value",
             getDomainAndIdentifier()
         );
-
-        File sensorValueDir = new File(PATH);
-        sensorValueDir.mkdir();
 
         dataRequester.setDataReceiveFunction(data -> {
             String fileName =
@@ -43,6 +56,8 @@ public class CSVService extends LocalCommunicationEndpoint {
                 System.out.println(fileName);
                 File csvDir = new File(fileName);
                 csvDir.mkdir();
+
+                savePicture(fileName);
 
                 FileWriter csvWriter = new FileWriter(fileName + "/measurement.csv", false);
                 csvWriter.append("x");
