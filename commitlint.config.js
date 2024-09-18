2a7b84576176293e42dcffe179944fc9f3ae2982
David P. Federl <david-peter.federl@stud.uni-due.de>
2024-09-18 10:18:58 +0200

    ci(workflow): update gitlab workflows
    - update used plugins
    - update required prettier plugins

--- commitlint.config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-    extends: ['@commitlint/config-conventional'],
-    rules: {
-        'type-enum': [2, "always", ['build', 'ci', 'feat', 'fix', 'docs', 'style', 'refactor', 'revert', 'chore', 'wip', 'perf']]
-    }
-}
