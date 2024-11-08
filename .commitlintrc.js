4ccad25ca6697dc7d63bb93be0b10249b4bc953e
David P. Federl <david-peter.federl@stud.uni-due.de>
2024-11-08 13:18:04 +0100

    build(all): update code checks

--- .commitlintrc.js
+++ .commitlintrc.js
@@ -4,16 +4,17 @@ module.exports = {
     "type-empty": [2, "never"],
     "type-case": [2, "always", "lower-case"],
     "type-enum": [
-      2,
+      1,
       "always",
       ["build", "ci", "feat", "fix", "docs", "style", "refactor", "revert", "chore", "wip", "perf"],
     ],
     "scope-empty": [2, "never"],
     "scope-case": [2, "always", "kebab-case"],
     "scope-enum": [
-      2,
+      1,
       "always",
       ["all", "application", "runtime", "workflow", "readme", "kubernetes", "docker"],
     ],
+    "subject-empty": [2, "never"],
   },
 };
