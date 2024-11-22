19de29a95ece562111f7461569a601056fa49640
David P. Federl <david-peter.federl@stud.uni-due.de>
2024-11-22 13:49:44 +0100

    docs(readme): update required JDK version

--- test_setup.sh
+++ test_setup.sh
@@ -9,10 +9,10 @@ fi
 
 if [[ -z $(command -v java) ]]; then
   UNDEFINED_DEPENDENCY_FOUND=1
-  echo "Java not found! Please install JDK version 17 or higher."
-elif [[ -z $(java --version | awk '/openjdk/ && ($2 == 17) {print "match"}') ]]; then
+  echo "Java not found! Please install JDK version 22."
+elif [[ -z $(java --version | awk '/openjdk/ && ($2 == 22) {print "match"}') ]]; then
   UNDEFINED_DEPENDENCY_FOUND=1
-  echo "Wrong version of Java found! Please install JDK version 17."
+  echo "Wrong version of Java found! Please install JDK version 22."
 fi
 
 if [[ -z $(command -v docker) ]]; then
